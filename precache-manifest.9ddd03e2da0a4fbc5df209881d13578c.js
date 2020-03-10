855e3e60bb3a1b575357b2a3ab651c297dc3df9b
MilanC994 <cabo.milan@yahoo.com>
2020-03-10 19:41:15 +0100

    Updates

---
+++ precache-manifest.9ddd03e2da0a4fbc5df209881d13578c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774b9ec169239e6ef3e1a7add4cd211b",
+    "url": "/SearchMovies/index.html"
+  },
+  {
+    "revision": "8eb2ed22633f0b53e06e",
+    "url": "/SearchMovies/static/css/main.2b74ff81.chunk.css"
+  },
+  {
+    "revision": "3fd4258376cf4870e2dc",
+    "url": "/SearchMovies/static/js/2.9f42083b.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/SearchMovies/static/js/2.9f42083b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb2ed22633f0b53e06e",
+    "url": "/SearchMovies/static/js/main.f5a2586d.chunk.js"
+  },
+  {
+    "revision": "edba97ba4e6cc8c8de5b",
+    "url": "/SearchMovies/static/js/runtime-main.24a42cb3.js"
+  }
+]);
